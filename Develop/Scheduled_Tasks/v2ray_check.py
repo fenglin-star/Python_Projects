2bce264ff59bbdd55049c160291cd923c698534a
fenglin-star <fenglin65@foxmail.com>
2021-05-08 07:55:00 +0800

    更新

--- Develop/Scheduled_Tasks/v2ray_check.py
+++ Develop/Scheduled_Tasks/v2ray_check.py
@@ -7,7 +7,6 @@ import json
 from multiprocessing import Pool
 
 
-list = []
 # 随机UserAgent
 fake_UserAgent = [
     "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
@@ -16,25 +15,6 @@ fake_UserAgent = [
     "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; zh-cn) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
 ]
-
-
-
-
-def requests_res(url,proxies):
-    # 如果遇到错误就出试5次
-    headers = {
-        "user-agent": random.choice(fake_UserAgent)
-    }
-    # requests 设置最多5次超时
-    s = requests.Session()
-    s.mount('http://', HTTPAdapter(max_retries=1))
-    s.mount('https://', HTTPAdapter(max_retries=1))
-    response = s.get(url,headers=headers, proxies=proxies, timeout=10)
-    response.encoding = response.apparent_encoding
-    html = response.text
-    return html
-
-
 
 def post_telegrambot(text):
     # 如果遇到错误就出试5次
@@ -56,6 +36,41 @@ def post_telegrambot(text):
             success_num = success_num + 1
             continue
 
+def requests_res(url):
+    try:
+        for i in range(0,3):
+            headers = {
+                "user-agent": random.choice(fake_UserAgent)
+            }
+            # requests 设置最多5次超时
+            s = requests.Session()
+            s.mount('http://', HTTPAdapter(max_retries=2))
+            s.mount('https://', HTTPAdapter(max_retries=2))
+            response = s.get(url,headers=headers,timeout=5)
+            print("检测第{}次  ".format(i),response.status_code,"  ",url)
+        return response.status_code
+
+    except Exception as e:
+        print("出现错误:", e)
+        return 404
+
+def get_dnspod_dns(domain):
+    post_data = {
+        "login_token": "216027,1406d367543fef1e471b78d894b379c2",
+        "format": "json",
+        "domain":domain,
+    }
+    # requests 设置最多五次超时
+    s = requests.Session()
+    s.mount('http://', HTTPAdapter(max_retries=3))
+    s.mount('https://', HTTPAdapter(max_retries=3))
+    res = s.post(
+        url="https://dnsapi.cn/Record.List",  # 翻译
+        data=post_data,timeout=20,)
+    data_txt = res.text
+    return data_txt
+
+
 
 def get_dnspod_ip(domain,name):
     post_data = {
@@ -71,12 +86,11 @@ def get_dnspod_ip(domain,name):
         url="https://dnsapi.cn/Record.List",  # 翻译
         data=post_data,timeout=20,)
     data_txt = json.loads(res.text)['records']
+
     for i in data_txt:
         if i['name'] == name:
             print('域名{}：'.format(domain),i['name'],i['value'],'id为:',i['id'],'line为:',i['line'])
             return i['value']  #返回查询域名现在的解析ip
-
-
 
 
 def modify_dnspod_ip(domain,record_id,sub_domain,value,record_type='CNAME'):
@@ -103,49 +117,46 @@ def modify_dnspod_ip(domain,record_id,sub_domain,value,record_type='CNAME'):
     return data_txt
 
 
-
 if __name__ == '__main__':
-    success_num = 0
-    while success_num < 6:
-        try:
-            for i in range(0,5):
-                proxies = {
-                    "http": "socks5://127.0.0.2:20808",
-                    'https': 'socks5://127.0.0.2:20808'
-                }
-                res = requests_res("http://ip-api.com/json/?lang=zh-CN", proxies)
-                print("正常状态 {}：".format(i),json.loads(res).get('query'), json.loads(res).get('country'), json.loads(res).get('regionName'))
+    nodehk_url = 'http://node-hk.2021214.xyz:30345/'
+    natcu_url = 'http://nat-cu.2021214.xyz:30345'
+    natcm_url = 'http://nat-cm.2021214.xyz:30345/'
 
-                # res = requests_res("http://119.28.49.98/", proxies)
-                # print("正常状态 {}：".format(i),res)
-
-            if get_dnspod_ip('2021214.xyz','fz')=='node-cm.2021214.xyz.':
-                print("node-cm.2021214.xyz 正常状态，无需更改")
-            else:
-                modify_dnspod_ip(domain='2021214.xyz',record_id='782528834',sub_domain='fz',
-                                 value='node-cm.2021214.xyz',record_type='CNAME')
-
-                # modify_dnspod_ip(domain='202014.xyz', record_id='737826514', sub_domain='node',
-                #                  value='node-cm.2021214.xyz', record_type='CNAME')
-
-                title = 'V2ray已修复，调整为node-cm.2021214.xyz'
-                print(title, success_num, post_telegrambot(text=title))
-            break
-
-        except Exception as e:
-            success_num = success_num + 1
-            print("正在重试:",success_num, e)
-            continue
+    if requests_res(nodehk_url) == 530:
+        print("{} 检测合格".format(nodehk_url))
+    else:
+        post_telegrambot(text='通道失效：{}'.format(nodehk_url))
 
 
-    if success_num >= 3:
-        title = 'V2ray存在问题'
-        if get_dnspod_ip('2021214.xyz', 'fz') == 'node-hk.2021214.xyz.':
-            print("node-hk.2021214.xyz 正常状态，无需更改")
+    if requests_res(natcu_url) == 530:
+        print("{} 检测合格".format(nodehk_url))
+        if get_dnspod_ip('2021214.xyz', 'node-cu') == 'nat-cu.2021214.xyz.':
+            print("node-cm.2021214.xyz 正常状态，无需更改")
         else:
-            print(title, success_num, post_telegrambot(text=title))
-            modify_dnspod_ip(domain='2021214.xyz', record_id='782528834', sub_domain='fz',
-                             value='node-hk.2021214.xyz', record_type='CNAME')
+            modify_dnspod_ip(domain='2021214.xyz', record_id='775458109', sub_domain='node-cu', value='nat-cu.2021214.xyz',
+                         record_type='CNAME')
+    else:
+        post_telegrambot(text='通道失效：{}'.format(natcu_url))
+        modify_dnspod_ip(domain='2021214.xyz', record_id='775458109', sub_domain='node-cm', value='node-hk.2021214.xyz',
+                         record_type='CNAME')
 
-            # modify_dnspod_ip(domain='202014.xyz', record_id='737826514', sub_domain='node',
-            #                  value='node-hk.2021214.xyz', record_type='CNAME')
+
+    if requests_res(natcm_url) == 530:
+        print("{} 检测合格".format(nodehk_url))
+        if get_dnspod_ip('2021214.xyz', 'node-cm') == 'nat-cm.2021214.xyz.':
+            print("node-cm.2021214.xyz 正常状态，无需更改")
+        else:
+            modify_dnspod_ip(domain='2021214.xyz', record_id='761863200', sub_domain='node-cm', value='nat-cm.2021214.xyz',
+                         record_type='CNAME')
+    else:
+        post_telegrambot(text='通道失效：{}'.format(natcm_url))
+        modify_dnspod_ip(domain='2021214.xyz', record_id='761863200', sub_domain='node-cm', value='node-hk.2021214.xyz',
+                         record_type='CNAME')
+
+
+
+
+
+
+
+
